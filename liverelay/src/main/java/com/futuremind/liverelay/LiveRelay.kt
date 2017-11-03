61ecd7378d57bb4cb62fb03a80954f8d11308f8c
Michał Klimczak <m.klimczak@futuremind.com>
2017-11-03 15:12:30 +0100

    make it possible to dispose on different lifecycle events (onPause being default)

--- liverelay/src/main/java/com/futuremind/liverelay/LiveRelay.kt
+++ liverelay/src/main/java/com/futuremind/liverelay/LiveRelay.kt
@@ -5,6 +5,7 @@ import com.jakewharton.rxrelay2.BehaviorRelay
 import florent37.github.com.rxlifecycle.RxLifecycle
 import io.reactivex.BackpressureStrategy.LATEST
 import io.reactivex.Flowable
+import io.reactivex.ObservableTransformer
 
 /**
  * LiveRelay can be used to process states between ViewModel and LifecycleOwner.
@@ -13,12 +14,26 @@ import io.reactivex.Flowable
  * onNext events.
  * Also, once observe is passed a valid [LifecycleOwner], subscribers don't need to worry about unsubscribing.
  */
-class LiveRelay<T> {
+class LiveRelay<T>(private val lifecycleEvent: LifecycleEvent = LifecycleEvent.PAUSE) {
 
     private val relay = BehaviorRelay.create<T>().toSerialized()
 
-    fun observe(lifecycleOwner: LifecycleOwner) : Flowable<T> = relay.compose(RxLifecycle.with(lifecycleOwner).disposeOnDestroy()).toFlowable(LATEST)
+    fun observe(lifecycleOwner: LifecycleOwner): Flowable<T> =
+            relay.compose(lifecycleTransformer(lifecycleEvent, lifecycleOwner)).toFlowable(LATEST)
 
     fun nextState(state: T) = relay.accept(state)
+
+    private fun lifecycleTransformer(lifecycleEvent: LifecycleEvent, lifecycleOwner: LifecycleOwner): ObservableTransformer<T, T> =
+            when (lifecycleEvent) {
+                LiveRelay.LifecycleEvent.PAUSE -> RxLifecycle.with(lifecycleOwner).disposeOnPause()
+                LiveRelay.LifecycleEvent.STOP -> RxLifecycle.with(lifecycleOwner).disposeOnStop()
+                LiveRelay.LifecycleEvent.DESTROY -> RxLifecycle.with(lifecycleOwner).disposeOnDestroy()
+            }
+
+    enum class LifecycleEvent {
+        PAUSE,
+        STOP,
+        DESTROY
+    }
 
 }
