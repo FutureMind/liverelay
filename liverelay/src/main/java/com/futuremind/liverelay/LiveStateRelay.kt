d67eb03c5789d9dee2cf8816a73718fd57360b0a
Michał Klimczak <m.klimczak@futuremind.com>
2018-02-24 09:55:51 +0100

    fix the docs for LiveRelays

--- liverelay/src/main/java/com/futuremind/liverelay/LiveStateRelay.kt
+++ liverelay/src/main/java/com/futuremind/liverelay/LiveStateRelay.kt
@@ -1,23 +1,24 @@
 package com.futuremind.liverelay
 
 import android.arch.lifecycle.LifecycleOwner
-import com.jakewharton.rxrelay2.PublishRelay
+import com.jakewharton.rxrelay2.BehaviorRelay
 import com.jakewharton.rxrelay2.Relay
 
 /**
- * [LiveStateRelay] can be used to process states between ViewModel and LifecycleOwner.
+ * [LiveStateRelay] can be used in MVVM for the View to observe states coming from ViewModel.
+ *
+ * When a new observer subscribes to [LiveStateRelay] it will be passed the last state if it exists
+ * (in contrast to [LiveEventRelay] which will only pass events registered after subscribing). This
+ * is desirable for application states, like results list being passed to the View - this way after
+ * resubscribing after e.g. configuration change the state will be recreated. Internally this is
+ * because it is based on [BehaviorRelay].
  *
  * Subscribers don't need to worry about unexpected onComplete or onError, it only passes onNext events.
- * Also, once observe is passed a valid [LifecycleOwner], subscribers don't need to worry about unsubscribing.
+ * Also, once [observe] is passed a valid [LifecycleOwner], subscribers don't need to worry about unsubscribing.
  *
- * The states passed to [LiveStateRelay] are volatile, which means that they are only passed to
- * existing subscribers, when a new observer subscribes it is not passed previous states
- * (in contrast to [LiveRelay] which will repeat its last state for consecutive subscribers).
- * Such behaviour may be desirable for volatile events, like Toast being shown.
- * Internally this is because it is based on [PublishRelay].
  */
 class LiveStateRelay<T> : BaseLiveRelay<T>() {
 
-    override val relay: Relay<T> = PublishRelay.create<T>().toSerialized()
+    override val relay: Relay<T> = BehaviorRelay.create<T>().toSerialized()
 
 }
