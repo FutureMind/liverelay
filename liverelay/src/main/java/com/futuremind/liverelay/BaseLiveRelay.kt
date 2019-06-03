2596757e1b46690659a30e76228eb66b695bd1e6
mskx42 <skotnickimarcel@gmail.com>
2019-06-03 16:23:14 +0200

    observer thread fix (#4)

--- liverelay/src/main/java/com/futuremind/liverelay/BaseLiveRelay.kt
+++ liverelay/src/main/java/com/futuremind/liverelay/BaseLiveRelay.kt
@@ -5,14 +5,14 @@ import com.jakewharton.rxrelay2.Relay
 import com.trello.rxlifecycle3.android.lifecycle.kotlin.bindToLifecycle
 import io.reactivex.BackpressureStrategy.LATEST
 import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 
 abstract class BaseLiveRelay<T> {
 
     protected abstract val relay: Relay<T>
 
-    fun observe(lifecycleOwner: LifecycleOwner): Flowable<T> =
-            relay.bindToLifecycle(lifecycleOwner).toFlowable(LATEST)
+    fun observe(lifecycleOwner: LifecycleOwner): Flowable<T> = relay.observeOn(AndroidSchedulers.mainThread()).bindToLifecycle(lifecycleOwner).toFlowable(LATEST)
 
     fun onNext(next: T) = relay.accept(next)
 
